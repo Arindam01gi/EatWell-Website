eb1a4b8c52c7d20bb57d04379fd353efb6ae0d7a
Arindam01gi <arindammaiti2018@gmail.com>
2022-11-05 10:38:31 +0530

    adding dynamic route features

--- app.js
+++ app.js
@@ -1,66 +1,32 @@
-const fs = require('fs');
-
 const path = require('path');
 
 const express = require("express");
 
 
 
+const defaultRoutes = require('./routes/default')
+const restaurantRoutes = require('./routes/restaurants')
+
 const app = express();
 
 app.set('views',path.join(__dirname , 'views'));
 app.set('view engine','ejs');
 
-app.use(express.static('public'))
+app.use(express.static('public'))  
 app.use(express.urlencoded({extended : false}));
 
-app.get("/",function(req,res){
-    res.render('index');
-})
-
-app.get('/restaurants',function(req,res){
-    const filePath =path.join(__dirname ,'data','restaurents.json');
-    const fileData=  fs.readFileSync(filePath);
-    const storeRestaurants = JSON.parse(fileData);
-    res.render('restaurants',{
-        numberOfRestaurants :storeRestaurants.length,
-        restaurants: storeRestaurants})
-})
-
-app.get('/restaurants/:id',function(req,res){
-    const restaurantId = req.params.id;
-    res.render('restaurant-detail',{rid: restaurantId})
-})
+app.use('/',defaultRoutes);
+app.use('/',restaurantRoutes);
 
 
-app.get('/recommend',function(req,res){
-  
-    res.render('recommend')
+
+
+app.use(function(req,res){
+    res.status(404).render('404');
 });
 
-app.post('/recommend', function(req,res){
-   const restaurant =  req.body;
-   const filePath =path.join(__dirname ,'data','restaurents.json');
-  const fileData=  fs.readFileSync(filePath);
-  const storeRestaurants = JSON.parse(fileData);
-
-storeRestaurants.push(restaurant);
-fs.writeFileSync(filePath, JSON.stringify(storeRestaurants));
-res.redirect('/confirm');
+app.use(function(error ,req,res,next){
+res.status().render('500');
 });
 
-app.get('/about',function(req,res){
-    
-    res.render('about')
-    
-})
-
-app.get('/confirm',function(req,res){
-   
-    res.render('confirm');
-})
-
-
-
-
-app.listen(3000);
+app.listen(3000 );
---
+++ util/restaurant-data.js
@@ -0,0 +1,24 @@
+
+const fs = require('fs');
+const path = require('path');
+
+
+const filePath =path.join(__dirname ,'..','data','restaurents.json');
+
+function getStoredRestaurants(){
+    
+    const fileData=  fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+
+    return storedRestaurants;
+}
+
+function storeRestaurants(storableRestaurants){
+    fs.writeFileSync(filePath, JSON.stringify(storableRestaurants));
+}
+
+module.exports ={
+   getStoredRestaurants : getStoredRestaurants,
+   storeRestaurants : storeRestaurants 
+};
